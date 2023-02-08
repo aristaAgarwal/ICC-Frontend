d27e3b2d824084411e974b212279af9ddc889938
mechalr8 <harsitagrawal59@gmail.com>
2023-02-08 21:31:50 +0530

    implemented google sigin using oAuth2.0

--- app/src/main/java/com/example/fintech/MainActivity.kt
+++ app/src/main/java/com/example/fintech/MainActivity.kt
@@ -1,10 +1,12 @@
 package com.example.fintech
 
+import android.app.Activity
 import android.content.Intent
 import android.os.Bundle
 import android.util.Log
 import android.widget.Button
 import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
 import com.google.android.gms.auth.api.signin.GoogleSignIn
 import com.google.android.gms.auth.api.signin.GoogleSignInAccount
@@ -14,60 +16,42 @@ import com.google.android.gms.tasks.Task
 
 
 class MainActivity : AppCompatActivity() {
-    var signInBtn : Button? = null
-
+    private var signInButton: Button? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        signInBtn =  findViewById(R.id.sign_in_button)
-
-        // Configure sign-in to request the user's ID, email address, and basic
-// profile. ID and basic profile are included in DEFAULT_SIGN_IN.
+        signInButton = findViewById(R.id.sign_in_button)
+//        signInButton?.setSize(SignInButton.SIZE_STANDARD)
 
         val gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+            .requestIdToken(BuildConfig.CLIENT_ID)
             .requestEmail()
             .build()
+        val mGoogleSignInClient = GoogleSignIn.getClient(this, gso)
+        val getResult = registerForActivityResult(ActivityResultContracts.StartActivityForResult()){
+            if(it.resultCode == Activity.RESULT_OK){
+                val task: Task<GoogleSignInAccount> = GoogleSignIn.getSignedInAccountFromIntent(it.data)
+                handleSignInResult(task)
+            }
+        }
 
-        // Build a GoogleSignInClient with the options specified by gso.
-       val mGoogleSignInClient = GoogleSignIn.getClient(this, gso)
-
-        signInBtn?.setOnClickListener {
+        signInButton?.setOnClickListener {
             val signInIntent: Intent = mGoogleSignInClient.signInIntent
-            startActivityForResult(signInIntent, 9001)
+            getResult.launch(signInIntent)
         }
     }
 
     override fun onStart() {
         super.onStart()
-        // Check for existing Google Sign In account, if the user is already signed in
-// the GoogleSignInAccount will be non-null.
         val account = GoogleSignIn.getLastSignedInAccount(this)
         updateUI(account)
     }
 
-    fun updateUI(account: GoogleSignInAccount?) {
-
-        if (account == null){
-            Toast.makeText(this,"Not signed in",Toast.LENGTH_SHORT).show()
-        }
-        else
-            Toast.makeText(this,"SIGNED IN",Toast.LENGTH_SHORT).show()
-    }
-    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
-        super.onActivityResult(requestCode, resultCode, data)
-
-        // Result returned from launching the Intent from GoogleSignInClient.getSignInIntent(...);
-        if (requestCode == 9001) {
-            // The Task returned from this call is always completed, no need to attach
-            // a listener.
-            val task: Task<GoogleSignInAccount> = GoogleSignIn.getSignedInAccountFromIntent(data)
-            handleSignInResult(task)
-        }
-    }
     private fun handleSignInResult(completedTask: Task<GoogleSignInAccount>) {
         try {
             val account = completedTask.getResult(ApiException::class.java)
+            val id = account.idToken
+            Log.e("idToken: ", id.toString())
 
             // Signed in successfully, show authenticated UI.
             updateUI(account)
@@ -76,6 +60,18 @@ class MainActivity : AppCompatActivity() {
             // Please refer to the GoogleSignInStatusCodes class reference for more information.
             Log.w("exp handleSignIn", "signInResult:failed code=" + e.statusCode)
             updateUI(null)
+        }
+    }
+
+    private fun updateUI(account: GoogleSignInAccount?) {
+        if (account == null) {
+            Toast.makeText(this,"Not signed in", Toast.LENGTH_SHORT).show()
+
+        } else {
+            Toast.makeText(this,"SIGNED IN",Toast.LENGTH_SHORT).show()
+            Log.e("msg signInSuccess", "SignIn Success")
+
+            Log.d("Account Info", account.toString())
         }
     }
 }
