bb7f92e7bb999695cfdeb946386458be8b1ad691
mechalr8 <harsitagrawal59@gmail.com>
2023-02-14 18:02:22 +0530

    Updated Login UI

--- app/src/main/java/com/example/fintech/LoginActivity.kt
+++ app/src/main/java/com/example/fintech/LoginActivity.kt
@@ -5,10 +5,12 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.view.View
 import android.widget.Button
 import android.widget.Toast
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.activity.viewModels
+import androidx.cardview.widget.CardView
 import androidx.lifecycle.ViewModel
 import androidx.recyclerview.widget.RecyclerView.ViewHolder
 import com.example.fintech.Model.IdToken
@@ -20,7 +22,7 @@ import com.google.android.gms.common.api.ApiException
 import com.google.android.gms.tasks.Task
 
 class LoginActivity : AppCompatActivity() {
-    private var signInButton: Button? = null
+    private var signInButton: CardView? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
