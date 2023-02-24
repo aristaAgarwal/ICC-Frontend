cd5ec77267031b7fdcbc5a60d0c69d6aa056670f
mechalr8 <harsitagrawal59@gmail.com>
2023-02-24 18:18:53 +0530

    "Bottom navigation completed"

--- app/src/main/java/com/example/fintech/UI/fragments/OtpFetch.kt
+++ app/src/main/java/com/example/fintech/UI/fragments/OtpFetch.kt
@@ -9,7 +9,6 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
-import com.example.fintech.model.Phone
 import com.example.fintech.model.VerifyOtpDO
 import com.example.fintech.UI.activities.MainActivity
 import com.example.fintech.constants.AppPreferences
