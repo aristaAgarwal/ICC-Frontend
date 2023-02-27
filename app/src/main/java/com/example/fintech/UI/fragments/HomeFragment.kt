0d67a8d3b50410272c04f13b65f4bfa4e24f8d3f
aristaAgarwal <aristaagarwal27@gmail.com>
2023-02-27 19:17:31 +0530

    "pageViewer added"

--- app/src/main/java/com/example/fintech/UI/fragments/HomeFragment.kt
+++ app/src/main/java/com/example/fintech/UI/fragments/HomeFragment.kt
@@ -1,23 +1,48 @@
 package com.example.fintech.UI.fragments
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.example.fintech.R
+import androidx.fragment.app.Fragment
+import androidx.viewpager2.widget.ViewPager2
+import com.example.fintech.adapter.ViewPagerAdapter
 import com.example.fintech.databinding.FragmentHomeBinding
+import com.google.android.material.tabs.TabLayoutMediator
+
 
 class HomeFragment : Fragment() {
 
     var binding: FragmentHomeBinding? = null
+    private lateinit var viewPager: ViewPager2
 
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
+        inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
+    ): View {
         // Inflate the layout for this fragment
         binding = FragmentHomeBinding.inflate(inflater, container, false)
         return binding!!.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding?.pager?.adapter = ViewPagerAdapter(this)
+
+        TabLayoutMediator(
+            binding!!.tabLayout, binding!!.pager
+        ) { tab, position ->
+            tab.text = when (position) {
+                0 -> "Matches"
+                1 -> "News"
+                2 -> "Videos"
+                else -> {
+                    ""
+                }
+            }
+        }.attach()
+    }
+
+    fun setPageViewAdapter() {
+
     }
 }
