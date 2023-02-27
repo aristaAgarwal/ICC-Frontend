80b060a12c3c6547f57d292ee6772d1794e632ac
mechalr8 <harsitagrawal59@gmail.com>
2023-02-27 15:53:18 +0530

    Parcelize

--- app/src/main/java/com/example/fintech/model/ProductsDO.kt
+++ app/src/main/java/com/example/fintech/model/ProductsDO.kt
@@ -1,8 +1,14 @@
 package com.example.fintech.model
 
+import android.os.Parcelable
+import kotlinx.parcelize.Parcelize
+import kotlinx.parcelize.RawValue
+
+@Parcelize
 data class ProductsDO(
     val code: Int,
     val data: AllProductsDO,
-    val message: Any,
+    val message: String?,
     val status: Boolean
-)
+): Parcelable
+
