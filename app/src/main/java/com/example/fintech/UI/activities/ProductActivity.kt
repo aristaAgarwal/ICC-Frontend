674ecee145717fe328be7e3812ba8490591a7e3f
aristaAgarwal <aristaagarwal27@gmail.com>
2023-02-27 22:17:45 +0530

    "product ui changes"

--- app/src/main/java/com/example/fintech/UI/activities/ProductActivity.kt
+++ app/src/main/java/com/example/fintech/UI/activities/ProductActivity.kt
@@ -1,13 +1,16 @@
 package com.example.fintech.UI.activities
 
+import android.graphics.Bitmap
+import android.graphics.drawable.Drawable
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
-import android.widget.TextView
-import com.example.fintech.R
+import android.widget.ImageView
+import androidx.annotation.Nullable
+import com.bumptech.glide.Glide
+import com.bumptech.glide.request.target.CustomTarget
+import com.bumptech.glide.request.transition.Transition
 import com.example.fintech.databinding.ActivityProductBinding
 import com.example.fintech.model.Product
-import com.ms.square.android.expandabletextview.ExpandableTextView
 
 class ProductActivity : AppCompatActivity() {
     lateinit var binding: ActivityProductBinding
@@ -16,6 +19,24 @@ class ProductActivity : AppCompatActivity() {
         binding = ActivityProductBinding.inflate(layoutInflater)
         setContentView(binding.root)
         val product = intent.getSerializableExtra("product") as Product
-        binding.expandableText.setText(product.description)
+        setContent(product)
+    }
+
+    fun setContent(product: Product) {
+        loadImage(product.display_image, binding.productImage)
+        binding.productTitle.text = product.name
+        binding.productDescription.text = product.description
+    }
+
+    fun loadImage(imageUrl: String, imageView: ImageView) {
+        Glide.with(this).asBitmap().load(imageUrl).into(object : CustomTarget<Bitmap?>() {
+            override fun onResourceReady(
+                resource: Bitmap, @Nullable transition: Transition<in Bitmap?>?
+            ) {
+                imageView.setImageBitmap(resource)
+            }
+
+            override fun onLoadCleared(@Nullable placeholder: Drawable?) {}
+        })
     }
 }
